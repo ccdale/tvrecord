6ad6464baefaeb8a9a2a091ffaaf8038a5add64a
ccdale <chris.allison@hivehome.com>
2022-07-23 12:21:57 +0100

    bring in config, set license banner etc, app name is now tvrecord

--- tvrecord/tvrecorddb/__init__.py
+++ tvrecord/tvrecorddb/__init__.py
@@ -1,3 +1,24 @@
+#
+# Copyright (c) 2022, Chris Allison
+#
+#     This file is part of tvrecord.
+#
+#     tvrecord is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     tvrecord is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with tvrecord.  If not, see <http://www.gnu.org/licenses/>.
+#
+"""tvrecorddb package for tvrecord app."""
+
+
 def searchZap(zap, search):
     try:
         poss = []
--- tvrecord/tvrecorddb/wrangler.py
+++ tvrecord/tvrecorddb/wrangler.py
@@ -1,22 +1,22 @@
 #
 # Copyright (c) 2022, Chris Allison
 #
-#     This file is part of tvrecorder.
+#     This file is part of tvrecord.
 #
-#     tvrecorder is free software: you can redistribute it and/or modify
+#     tvrecord is free software: you can redistribute it and/or modify
 #     it under the terms of the GNU General Public License as published by
 #     the Free Software Foundation, either version 3 of the License, or
 #     (at your option) any later version.
 #
-#     tvrecorder is distributed in the hope that it will be useful,
+#     tvrecord is distributed in the hope that it will be useful,
 #     but WITHOUT ANY WARRANTY; without even the implied warranty of
 #     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #     GNU General Public License for more details.
 #
 #     You should have received a copy of the GNU General Public License
-#     along with tvrecorder.  If not, see <http://www.gnu.org/licenses/>.
+#     along with tvrecord.  If not, see <http://www.gnu.org/licenses/>.
 #
-"""Data wrangler module for tvrecorder."""
+"""Data wrangler module for tvrecorddb."""
 import sys
 import time
 
