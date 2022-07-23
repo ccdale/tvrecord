262e3a071b006fd2d4f2863d0b37609a3aea4959
ccdale <chris.allison@hivehome.com>
2022-07-23 12:34:10 +0100

    tvrecord begin() function
    errors importing the db.py file

--- tvrecord/__init__.py
+++ tvrecord/__init__.py
@@ -1,1 +1,37 @@
-__version__ = '0.1.0'
+#
+# Copyright (c) 2022, Christopher Allison
+#
+#     This file is part of tvrecord.
+#
+#     tvrecord is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     tvrecord is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with tvrecord.  If not, see <http://www.gnu.org/licenses/>.
+"""package module for tvrecord."""
+import sys
+
+from ccaerrors import errorNotify, errorExit
+from tvrecord.config import Configuration
+from tvrecord.tvrecorddb.db import makeDBEngine
+
+__version__ = "0.1.0"
+
+appname = "tvrecord"
+
+
+def begin(debug=False):
+    """Starts a connection to the db and returns the db engine and the app config."""
+    try:
+        cf = Configuration(appname=appname)
+        eng = makeDBEngine(cf, echo=debug)
+        return (cf, eng)
+    except Exception as e:
+        errorExit(sys.exc_info()[2], e)
---
+++ tvrecord/tvrecorddb/db.py
@@ -0,0 +1,63 @@
+#
+# Copyright (c) 2022, Chris Allison
+#
+#     This file is part of tvrecord.
+#
+#     tvrecord is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     tvrecord is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with tvrecord.  If not, see <http://www.gnu.org/licenses/>.
+#
+"""db module for tvrecord"""
+import sys
+
+from ccaerrors import errorNotify, errorExit
+from sqlalchemy import create_engine
+
+from tvrecord.config import Configuration
+from tvrecord.models import Base
+
+
+def makeDBCreds(cf):
+    try:
+        keys = ["dbhost", "dbdb", "dbuser", "dbpass"]
+        op = {}
+        for key in keys:
+            val = cf.get(key)
+            if val is None:
+                raise Exception(f"setting {key} not found in configuration")
+            op[key] = val
+        return op
+    except Exception as e:
+        errorExit(sys.exc_info()[2], e)
+
+
+def makeDBEngine(cf, echo=False):
+    try:
+        creds = makeDBCreds(cf)
+        connstr = f'mysql+pymysql://{creds["dbuser"]}:{creds["dbpass"]}'
+        connstr += f'@{creds["dbhost"]}/{creds["dbdb"]}'
+        return create_engine(connstr, echo=echo)
+    except Exception as e:
+        errorNotify(sys.exc_info()[2], e)
+
+
+def createTables(engine):
+    try:
+        Base.metadata.create_all(engine)
+    except Exception as e:
+        errorNotify(sys.exc_info()[2], e)
+
+
+if __name__ == "__main__":
+    cf = Configuration(appname="tvrecord")
+    mysqleng = makeDBEngine(cf)
+    createTables(mysqleng)
