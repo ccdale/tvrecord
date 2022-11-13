c7ca232b56b09a3614256544e7fe89b5ee5e7229
ccdale <chris.charles.allison@gmail.com>
2022-11-13 09:42:29 +0000

    move record function to dvbctrl

--- tvrecord/tvrecorder/monitor.py
+++ tvrecord/tvrecorder/monitor.py
@@ -1,15 +1,32 @@
+import os
 from signal import signal, SIGINT, SIGTERM, SIGHUP
 import sys
 from threading import Event
+import time
 
 from ccaerrors import errorNotify, errorRaise, errorExit
+import ccalogging
 from dvbctrl.commands import DVBCommand
 from dvbctrl.dvbstreamer import DVBStreamer
+from dvbctrl.recorder import Recorder
+from slugify import slugify
 
 import tvrecord
-import tvrecord.tvrecorder as recorder
+from tvrecord.tvrecorddb.wrangler import getScheduleRecord
 
-log = recorder.log
+__appname__ = "tvrecord"
+home = os.path.expanduser("~/")
+logd = os.path.join(home, "log")
+res = os.makedirs(logd, exist_ok=True)
+logfn = os.path.join(logd, f"{__appname__}.log")
+ccalogging.setLogFile(logfn, rotation=30)
+ccalogging.setInfo()
+log = ccalogging.log
+
+fnadapters = os.listdir("/dev/dvb/")
+adapters = [int(x.replace("adapter", "")) for x in fnadapters]
+streamers = [DVBStreamer(x) for x in adapters]
+
 doexit = Event()
 
 
@@ -28,18 +45,123 @@ signal(SIGTERM, interruptMonitor)
 signal(SIGHUP, interruptMonitor)
 
 
-def doTasks(cf, eng):
-    """iterate through the tasks"""
+def startUp():
+    """Start each streamer
+
+    tune each adapter to a different mux
+    """
     try:
-        pass
+        log.debug(f"starting {len(adapters)} streamers")
+        [s.start() for s in streamers if not s.isRunning()]
+        dvbc = DVBCommand(adapters[0])
+        muxes = dvbc.lsmuxes()
+        log.debug(f"mux list: {muxes}")
+        for cn, a in enumerate(adapters):
+            dvbc = DVBCommand(a)
+            chans = dvbc.lsservices(muxes[cn])
+            log.debug(f"chan list for mux {muxes[cn]}: {chans}")
+            if len(chans):
+                log.info(f"Tuning adapter {a} to mux {muxes[cn]} (channel {chans[0]})")
+                if not dvbc.tuneToChannel(chans[0]):
+                    log.error(
+                        f"Failed to tune to channel {chans[0]} on mux {muxes[cn]}"
+                    )
     except Exception as e:
         errorNotify(sys.exc_info()[2], e)
 
 
-def shutDown():
+def shutDown(cf):
     try:
+        log.debug("writing config")
         cf.writeConfig()
-        recorder.stopAll()
+        log.info("stopping dvbstreamers")
+        [s.stop() for s in streamers if s.isRunning()]
+        for s in streamers:
+            if s.isRunning():
+                log.error(f"Failed to stop streamer on adapter {s.adapter}")
+            else:
+                log.debug(f"Stopped streamer on adapter {s.adapter}")
+    except Exception as e:
+        errorNotify(sys.exc_info()[2], e)
+
+
+def findFreeAdapter():
+    try:
+        free = None
+        for s in streamers:
+            dvbc = DVBCommand(s.adapter)
+            output = dvbc.getmrl()
+            if output[0].startswith("null"):
+                return s.adapter
+        return None
+    except Exception as e:
+        errorNotify(sys.exc_info()[2], e)
+
+
+def muxForChannel(channel):
+    # any adapter will do, pick the first off of the list
+    try:
+        dvbc = DVBCommand(str(adapters[0]))
+        dvbc.open()
+        lines = dvbc.serviceinfo(channel)
+        dvbc.close()
+        return lines[0]
+    except Exception as e:
+        errorNotify(sys.exc_info()[2], e)
+
+
+def nextStart(upcoming, startpad, endpad):
+    try:
+        now = time.time()
+        nextstart = upcoming[0]["schedule"]["airdate"] - cf.get("startpad")
+        if nextstart <= now:
+            return upcoming[0]
+        else:
+            return None
+    except Exception as e:
+        errorNotify(sys.exc_info()[2], e)
+
+
+def startRecording(nextrecording):
+    try:
+        adapter = findFreeAdapter()
+        if adapter is not None:
+            pass
+    except Exception as e:
+        errorNotify(sys.exc_info()[2], e)
+
+
+def makeFileName(nextrecording):
+    try:
+        program = nextrecording["program"]
+        txt = program["title"]
+        if "episodetitle" in program and len(program["episodetitle"]):
+            txt = f"{txt}-{program['episodetitle']}"
+        if "series" in program and program["series"] is not None:
+            sp = program["series"]
+            s = f"S0{sp}" if sp < 10 else f"S{sp}"
+            if "episode" in program and program["episode"] is not None:
+                ep = program["episode"]
+                e = f"E0{ep}" if ep < 10 else f"E{ep}"
+            else:
+                e = ""
+            txt = f"{txt}-{s}{e}"
+        title = slugify(txt)
+        # add date and time to beginning
+        # .ts to end
+
+    except Exception as e:
+        errorNotify(sys.exc_info()[2], e)
+
+
+def doTasks(cf, eng):
+    """iterate through the tasks"""
+    try:
+        upcoming = getScheduleRecord(eng)
+        if len(upcoming):
+            ns = nextStart(upcoming, cf.get("startpad"), cf.get("endpad"))
+            if ns is not None:
+                pass  # start recording
     except Exception as e:
         errorNotify(sys.exc_info()[2], e)
 
@@ -48,15 +170,18 @@ def monitor(debug=False):
     """wake up, kick off any task, sleep, repeat"""
     try:
         log.info(f"DVB Monitor version {tvrecord.__version__} starting")
-        if len(recorder.streamers) == 0:
+        if len(streamers) == 0:
             raise Exception("No DVBStreamers found")
-        recorder.startAll()
+        startUp()
+        log.info(f"found, started and tuned {len(streamers)} DVBStreamers")
+        mfc = muxForChannel("Sky News")
+        log.debug(f"{mfc=}")
         cf, eng = tvrecord.begin(debug=debug)
         sleeptime = int(cf.get("sleeptime", "60"))
         log.debug("while loop starting, hello.")
         while not doexit.is_set():
             doTasks(cf, eng)
-            log.debug(f"Waiting for {sleeptime} seconds")
+            # log.debug(f"Waiting for {sleeptime} seconds")
             doexit.wait(sleeptime)
         log.debug("while loop exiting, bye")
         log.info("Shutting down DVB Monitor")
@@ -66,4 +191,5 @@ def monitor(debug=False):
 
 
 if __name__ == "__main__":
+    ccalogging.setDebug()
     monitor(debug=True)
