50ab3ace858f5414bda53ead16117486d8706e50
ccdale <chris.charles.allison@gmail.com>
2022-11-20 10:49:02 +0000

    human readable date time

--- tvrecord/tvrecorder/monitorrecorder.py
+++ tvrecord/tvrecorder/monitorrecorder.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import sys
 
 from ccaerrors import errorNotify, errorRaise, errorExit
@@ -17,9 +18,13 @@ class MonitorRecorder(Recorder):
 
     def __repr__(self):
         try:
+            dt = datetime.fromtimestamp(self.starttime)
+            st = dt.strftime("%Y-%m-%d %H:%M:%S")
+            dt = datetime.fromtimestamp(self.endtime)
+            et = dt.strftime("%Y-%m-%d %H:%M:%S")
             msg = f"<MonitorRecorder({self.channel}, {self.fqfn}"
-            msg = f"{msg}, adapter={self.dvbc.adapter}, starttime={self.starttime}"
-            msg = f"{msg}, endtime={self.endtime})>"
+            msg = f"{msg}, adapter={self.dvbc.adapter}, starttime={st}"
+            msg = f"{msg}, endtime={et})>"
             return msg
         except Exception as e:
             errorNotify(sys.exc_info()[2], e)
